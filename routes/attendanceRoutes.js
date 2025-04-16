1d446a6219d51ed4bbf0b334e39b10cb6df0d32b
Jay <110962603+JayV0128@users.noreply.github.com>
2025-04-16 23:09:46 +0800

    Delete routes directory

--- routes/attendanceRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const attendanceController = require('../controllers/attendanceController');
-
-router.post('/log', attendanceController.logAttendance);
-router.get('/logs', attendanceController.getAllLogs);
-
-module.exports = router;
--- routes/userRoutes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const userController = require('../controllers/userController');
-
-router.post('/create', userController.createUser);
-
-module.exports = router;
