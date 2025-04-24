55d0ff2c027cd0898199e149decff3b20ceaddbc
Jay <110962603+JayV0128@users.noreply.github.com>
2025-04-25 01:44:06 +0800

    Add files via upload

---
+++ tests/stress_test_c5l.js
@@ -0,0 +1,31 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '10s', target: 50 },  
+    { duration: '30s', target: 50 },
+    { duration: '10s', target: 0 },
+  ],
+};
+
+export default function () {
+  const url = 'http://attendance-sys-c5l-env.eba-4pjm59av.us-east-1.elasticbeanstalk.com/attendance/log';
+  const payload = JSON.stringify({
+    token: `test-nfc-${__VU}`,
+    method: 'NFC',
+    deviceInfo: "Web"              
+  });
+
+  const params = {
+    headers: { 'Content-Type': 'application/json' },
+  };
+
+  const res = http.post(url, payload, params);
+
+  check(res, {
+    'status is 201': (r) => r.status === 201,
+  });
+
+  sleep(1);
+}
